1a2af85f07814bcd65a05613bc0bd6a73cb0fba2
todorov <todorov.anatole@gmail.com>
2021-09-07 17:59:43 +0200

    See Description for usage of keys
    -On left click : on an enemy, display his possible Mvt, on a empty tile deselect a selected allie, on an allie display his possible mvt, on a highlight allie tile setup waypoints for the path
    -On Right click : on an empty tile deselect a selected allie, on an allie reset all waypoint
    - Space, launch the PlayMode, all character will move according to waypoints, once everyone finish go back to TacticalMode

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -25,7 +25,7 @@ public class Character : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -33,4 +33,5 @@ public class Character : MonoBehaviour
     {
         
     }
+
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] int length = 20;
-    [SerializeField] int height = 14;
-    [SerializeField] Camera cam = null;
+    [SerializeField] int   length = 20;
+    [SerializeField] int   height = 14;
+    [SerializeField] float speed = 10;
 
+    [SerializeField] Camera cam = null;
     [SerializeField] GameObject map = null;
     [SerializeField] GameObject highlightTileParent = null;
     [SerializeField] GameObject gridParent = null;
@@ -15,6 +16,10 @@ public class GameManager : MonoBehaviour
 
     Transform selectedEntity = null;
     List<int> indexHighlightTiles = new List<int>();
+
+
+    bool inPlayMode = false;
+    [SerializeField] List<GameObject> characters = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
@@ -27,38 +32,109 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (inPlayMode)
+            PlayMode();
+        else
+            TacticalMode();
+
+    }
+
+
+    void PlayMode()
+    {
+        foreach (GameObject character in characters)
+        {
+            Character characterScript = character.GetComponent<Character>();
+
+            if (characterScript.queueTileIndex.Count > 0)
+            {
+                Vector2 tilePos = GetPosFromTile(characterScript.queueTileIndex[0]);
+                Vector3 direction = new Vector3(tilePos.x, tilePos.y, character.transform.position.z) - character.transform.position;
+
+
+                character.transform.position += direction.normalized * (speed * Time.deltaTime);
+
+                //if you pass the waypoint remove it
+                if (Vector3.Dot(direction, new Vector3(tilePos.x, tilePos.y, character.transform.position.z) - character.transform.position) < 0)
+                {
+                    characterScript.queueTileIndex.RemoveAt(0);
+                    UpdateTrailPath(character);
+                }
+                else
+                    character.GetComponent<LineRenderer>().SetPosition(0, character.transform.position);
+
+            }
+        }
+
+        if (!isThereStillWaypoints())
+            inPlayMode = false;
+    }
+    void TacticalMode()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            inPlayMode = true;
+
+
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
-            
+
             if (Physics.Raycast(ray, out hit))
             {
+
+
                 //select Allies
                 if (hit.transform.CompareTag("Allies"))
-                {                   
-                    GenerateHighlightTiles(GetTile(hit.point.x, hit.point.y), hit.transform.GetComponent<Character>().mvt, true);
+                {
+                    //to check if display highlight tiles from character or last waypoint
                     selectedEntity = hit.transform;
+                    Character character = selectedEntity.GetComponent<Character>();
+                    GenerateHighlightTiles(character.queueTileIndex.Count == 0 ? GetTile(hit.point.x, hit.point.y) : character.queueTileIndex[character.queueTileIndex.Count - 1], character.mvt, true);
+
                 }
                 //select Enemies
                 else if (hit.transform.CompareTag("Enemies"))
                 {
                     GenerateHighlightTiles(GetTile(hit.point.x, hit.point.y), hit.transform.GetComponent<Character>().mvt, false);
+                    selectedEntity = null;
                 }
                 //select a Tile
                 else
                 {
                     int tileIndex = GetTile(hit.point.x, hit.point.y);
 
-
-                    if (selectedEntity != null && indexHighlightTiles.Contains(tileIndex) )
+                    if (selectedEntity != null && indexHighlightTiles.Contains(tileIndex))
                     {
-                        Vector2 temp = GetPosFromTile(tileIndex);
-                        selectedEntity.position = new Vector3(temp.x, temp.y, selectedEntity.position.z);
-                    }
+                        Character character = selectedEntity.GetComponent<Character>();
+                        int referenceTile = character.queueTileIndex.Count == 0 ? GetTile(selectedEntity.position.x, selectedEntity.position.y) : character.queueTileIndex[character.queueTileIndex.Count - 1];
 
-                    ClearHighlightTiles();
+                        int offsetX = GetOffsetXBetweenTiles(referenceTile, tileIndex);
+                        int offsetY = GetOffsetYBetweenTiles(referenceTile, tileIndex);
+
+                        for (int i = 1; i < Mathf.Abs(offsetX) + 1; ++i)
+                        {
+                            character.queueTileIndex.Add(referenceTile + ((offsetX > 0) ? i : -i));
+                        }
+
+                        //Reset reference tile because tiles maybe have been add
+                        referenceTile = character.queueTileIndex.Count == 0 ? GetTile(selectedEntity.position.x, selectedEntity.position.y) : character.queueTileIndex[character.queueTileIndex.Count - 1];
+
+                        for (int i = 1; i < Mathf.Abs(offsetY) + 1; ++i)
+                        {
+                            character.queueTileIndex.Add(referenceTile + length * ((offsetY > 0) ? i : -i));
+                        }
+
+                        GenerateHighlightTiles(character.queueTileIndex.Count == 0 ? GetTile(selectedEntity.position.x, selectedEntity.position.y) : character.queueTileIndex[character.queueTileIndex.Count - 1], character.mvt, true);
+                        UpdateTrailPath(selectedEntity.gameObject);
+                    }
+                    else
+                    {
+                        ClearHighlightTiles();
+                        selectedEntity = null;
+                    }
 
                 }
 
@@ -68,13 +144,41 @@ public class GameManager : MonoBehaviour
 
         if (Input.GetMouseButtonDown(1))
         {
-            ClearHighlightTiles();
-        }
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
 
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.CompareTag("Allies"))
+                {
+                    selectedEntity = hit.transform;
+                    ClearTrailPath(selectedEntity.gameObject);
+                }
+                else
+                    ClearHighlightTiles();
+            }
+            else
+                ClearHighlightTiles();
+        }
 
     }
 
 
+
+
+    //Playmode
+    bool isThereStillWaypoints()
+    {
+        foreach (GameObject character in characters)
+            if (character.GetComponent<Character>().queueTileIndex.Count > 0)
+                return true;
+
+        return false;
+    }
+
+
+    //Grid
     void GenerateGrid()
     {
         //clear previous lines
@@ -105,6 +209,7 @@ public class GameManager : MonoBehaviour
 
 
     }
+    //Highlight
     void GenerateHighlightTiles(int CentralTile, int statMvt, bool isAllies)
     {
         ClearHighlightTiles();
@@ -120,10 +225,7 @@ public class GameManager : MonoBehaviour
                 if (CentralTile == tileIndex)
                     continue;
 
-                int offsetX = Mathf.Abs(CentralTile % length - tileIndex % length);
-                int offsetY = Mathf.Abs((int)(CentralTile / length) - (int)(tileIndex / length));
-
-                if (offsetX + offsetY <= statMvt)
+                if (GetOffsetAllBetweenTiles(CentralTile, tileIndex) <= statMvt - (isAllies ? selectedEntity.GetComponent<Character>().queueTileIndex.Count : 0))
                 {
                     indexHighlightTiles.Add(tileIndex);
                     GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
@@ -140,17 +242,47 @@ public class GameManager : MonoBehaviour
             Destroy(child.gameObject);
 
         indexHighlightTiles.Clear();
-        selectedEntity = null;
     }
-
+    //Tiles
     int GetTile(float x, float y)
-    { 
-        return Mathf.RoundToInt(x + 0.5f * (length - 1)) + Mathf.RoundToInt(y + 0.5f * (height - 1)) * length; 
+    {
+        return Mathf.RoundToInt(x + 0.5f * (length - 1)) + Mathf.RoundToInt(y + 0.5f * (height - 1)) * length;
     }
     Vector2 GetPosFromTile(int tileIndex)
     {
-        return new Vector2(-0.5f * (length - 1) + tileIndex % length, 
+        return new Vector2(-0.5f * (length - 1) + tileIndex % length,
                            -0.5f * (height - 1) + (int)(tileIndex / length));
+    }
+    int GetOffsetAllBetweenTiles(int tile1, int tile2) { return Mathf.Abs(GetOffsetXBetweenTiles(tile1, tile2)) + Mathf.Abs(GetOffsetYBetweenTiles(tile1, tile2)); }
+    int GetOffsetXBetweenTiles(int tile1, int tile2) { return tile2 % length - tile1 % length;  }
+    int GetOffsetYBetweenTiles(int tile1, int tile2) { return (int)(tile2 / length) - (int)(tile1 / length); }
+    //Trail
+    void UpdateTrailPath(GameObject character)
+    {
+        Character characterScript = character.GetComponent<Character>();
+        LineRenderer lr = character.GetComponent<LineRenderer>();
+
+        //set to 0 to reset all
+        lr.positionCount = 0;
+        lr.positionCount = characterScript.queueTileIndex.Count + 1;
+        lr.SetPosition(0, character.transform.position);
+
+        for (int i = 0; i < characterScript.queueTileIndex.Count; ++i)
+        {
+            Vector2 linePos = GetPosFromTile(characterScript.queueTileIndex[i]);
+            lr.SetPosition(i + 1, new Vector3(linePos.x, linePos.y, -0.53f));
+        }
+
+    }
+    void ClearTrailPath(GameObject character)
+    {
+        Character characterScript = character.GetComponent<Character>();
+        LineRenderer lr = character.GetComponent<LineRenderer>();
+
+        characterScript.queueTileIndex.Clear();
+        lr.positionCount = 0;
+
+        selectedEntity = null;
     }
 
 }
