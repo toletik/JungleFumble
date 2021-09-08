2aa4b18bf13e624666e63085f74de5f8eb6ad18a
todorov <todorov.anatole@gmail.com>
2021-09-08 12:53:42 +0200

    Add all behaviors for the ball
    Add all behaviors for the ball

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -20,18 +20,35 @@ public class Character : MonoBehaviour
     [SerializeField] public int precision = 0;
     [SerializeField] public int catchUp = 0;
 
+    [SerializeField] public GameObject ballIcon = null;
+    public bool hasBall = false;
+    public bool canPickUpBall = true;
+
+    public Vector3 initialPos;
     public List<int> queueTileIndex = new List<int>();
 
     // Start is called before the first frame update
     void Start()
     {
-
+        initialPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball") && canPickUpBall)
+        {
+            hasBall = true;
+            ballIcon.SetActive(true);
+            other.GetComponent<LineRenderer>().positionCount = 0;
+            other.gameObject.SetActive(false);
+        }
     }
 
 }
